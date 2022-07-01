91f1843b0760654cb1602995d398f5c5edefcc9e
edomos <alf.ramoz@gmail.com>
2022-07-01 00:49:17 -0700

    create input fields and implement google maps api -   address input field -   latitude/longitude input field -   google geocoding + google map apis -   include urls to fetch lat/long and usgs design values

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import './App.css';
+import { useJsApiLoader, GoogleMap } from '@react-google-maps/api';
+import { useEffect } from 'react';
 
+let lat = 33.76672;
+let lng = -118.1924;
+let riskCategory = 'I';
+let siteClass = 'D-default';
+let title = 'Long Beach';
 
-const apiKey = process.env.API_KEY;
+const apiKey = process.env.REACT_APP_API_KEY;
+let addressToLatLong_URL = `https://maps.googleapis.com/maps/api/geocode/json?address=324+south+ave+long+beach+ca&key=${apiKey}`;
+let latLongToAddress_URL = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${apiKey}`;
+let designGroundMotion_URL = `https://earthquake.usgs.gov/ws/designmaps/asce7-16.json?latitude=${lat}&longitude=${lng}&riskCategory=${riskCategory}&siteClass=${siteClass}&title=${title}`;
 
 function App() {
+  //Fetch USGS Info (Mainly Ss, S1)
+  useEffect(() => {
+    fetch(designGroundMotion_URL)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, [designGroundMotion_URL]);
+
+  //Google Map Stuff
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_API_KEY, //Cant figure out why process.env removes waterlogo, works fine if api key is hardcoded?
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="app">
-    <input type="text" className='address' placeholder='address'/>
-    <input type="text" className='input__lat' placeholder='latitude'/>
-    <input type="text" className='input__long' placeholder='longitude'/>
+      <div className="input__address--container">
+        <input type="text" className="address" placeholder="address" className="input__address"/>
+      </div>
+      <div className="input__latlng--container">
+        <input type="text" className="input__lat" placeholder="latitude" className="input__latlng" />
+        <input type="text" className="input__lng" placeholder="longitude" className="input__latlng"/>
+      </div>
+      <div className="map__container">
+        <GoogleMap
+          center={{ lat: 33.82, lng: -118.17 }}
+          zoom={10}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+        >
+        </GoogleMap>
+      </div>
     </div>
   );
 }
 
 export default App;
-
